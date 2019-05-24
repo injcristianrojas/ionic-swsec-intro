eb47194b086dbb90126c597da3a21bc2963b6a01
Cristián Rojas <injcristianrojas@gmail.com>
2019-05-24 12:47:47 -0400

    Cleanup

--- src/app/posts/posts.page.ts
+++ src/app/posts/posts.page.ts
@@ -7,25 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsPage implements OnInit {
   private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  public items: Array<{ title: string; note: string; }> = [];
   constructor() {
     for (let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
         note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
   }
